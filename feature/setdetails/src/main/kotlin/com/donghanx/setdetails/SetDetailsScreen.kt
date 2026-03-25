0a7b12a4dc918adf1e5377ab9da3529e66bec6a9
--global <donghan.xia00@gmail.com>
2026-03-25 17:13:02 -0500

    Add ColorFilter for SetIcon to render the SVG by adapting the current light/dark mode tint

--- feature/setdetails/src/main/kotlin/com/donghanx/setdetails/SetDetailsScreen.kt
+++ feature/setdetails/src/main/kotlin/com/donghanx/setdetails/SetDetailsScreen.kt
@@ -40,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
@@ -241,7 +242,12 @@ private fun SetDetailsTitle(name: String, iconUri: String, modifier: Modifier = 
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
     ) {
-        AsyncImage(model = iconUri, contentDescription = name, modifier = Modifier.size(24.dp))
+        AsyncImage(
+            model = iconUri,
+            contentDescription = name,
+            colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.onSurface),
+            modifier = Modifier.size(24.dp),
+        )
 
         Spacer(modifier = Modifier.width(4.dp))
 
